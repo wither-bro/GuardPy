7d45850346a9c7b7066ad69febb9ba73f0cc53d3
wither-bro <alexpoloian@gmail.com>
2026-06-05 11:54:29 +0400

    Update name.cpp

--- App/Page 2 ,Create Account,/Text Box/Name Box/name.cpp
+++ App/Page 2 ,Create Account,/Text Box/Name Box/name.cpp
@@ -7,6 +7,8 @@ private:
     sf::Font font;
     sf::Text text;
     std::string input;
+    std::string placeholder = "Enter your username";
+    bool isFocused = false;
 
 public:
     NameBox(const std::string& fontPath, float x, float y) {
@@ -14,24 +16,37 @@ public:
 
         box.setSize({300.f, 40.f});
         box.setPosition(x, y);
-        box.setFillColor(sf::Color(40, 0, 80));
+        box.setFillColor(sf::Color::Black);
         box.setOutlineThickness(1.f);
         box.setOutlineColor(sf::Color::White);
 
         text.setFont(font);
         text.setCharacterSize(20);
-        text.setFillColor(sf::Color::White);
         text.setPosition(x + 10.f, y + 8.f);
+        text.setFillColor(sf::Color(150, 150, 150));
+        text.setString(placeholder);
     }
 
-    void handleInput(sf::Event& event) {
-        if (event.type == sf::Event::TextEntered) {
+    void handleEvent(sf::Event& event, sf::RenderWindow& window) {
+        if (event.type == sf::Event::MouseButtonPressed) {
+            sf::Vector2f mousePos(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y));
+            isFocused = box.getGlobalBounds().contains(mousePos);
+        }
+
+        if (isFocused && event.type == sf::Event::TextEntered) {
             if (event.text.unicode == 8) {
                 if (!input.empty()) input.pop_back();
-            } else if (event.text.unicode < 128) {
+            } else if (event.text.unicode >= 32 && event.text.unicode < 127) {
                 input += static_cast<char>(event.text.unicode);
             }
-            text.setString(input);
+
+            if (input.empty()) {
+                text.setString(placeholder);
+                text.setFillColor(sf::Color(150, 150, 150));
+            } else {
+                text.setString(input);
+                text.setFillColor(sf::Color::White);
+            }
         }
     }
 
