2a6345af265665196b2732fab98aa0e894a6d322
wither-bro <alexpoloian@gmail.com>
2026-06-06 13:41:45 +0400

    Create api_key_no_chat.cpp

---
+++ App/Page 2 ,Create Account,/Page 3 ,Developer Menu,/Glasses/Grey/Menus/Menu Choice/api_key_no_chat.cpp
@@ -0,0 +1,54 @@
+#include <SFML/Graphics.hpp>
+#include <string>
+
+class ApiKeyNoChat {
+private:
+    sf::RectangleShape box;
+    sf::Text titleText;
+    sf::RectangleShape button;
+    sf::Text buttonText;
+    sf::Font font;
+
+public:
+    ApiKeyNoChat(const std::string& fontPath, float x, float y) {
+        font.loadFromFile(fontPath);
+
+        box.setSize(sf::Vector2f(300.f, 150.f));
+        box.setFillColor(sf::Color(255, 255, 255, 50));
+        box.setPosition(x, y);
+
+        titleText.setFont(font);
+        titleText.setString("Get API Key without Chat");
+        titleText.setCharacterSize(18);
+        titleText.setFillColor(sf::Color::White);
+        titleText.setPosition(x + 20.f, y + 20.f);
+
+        button.setSize(sf::Vector2f(80.f, 30.f));
+        button.setFillColor(sf::Color(128, 0, 128));
+        button.setPosition(x + 110.f, y + 90.f);
+
+        buttonText.setFont(font);
+        buttonText.setString("Get");
+        buttonText.setCharacterSize(16);
+        buttonText.setFillColor(sf::Color::White);
+        
+        sf::FloatRect textRect = buttonText.getLocalBounds();
+        buttonText.setOrigin(textRect.width / 2.0f, textRect.height / 2.0f);
+        buttonText.setPosition(x + 150.f, y + 105.f);
+    }
+
+    bool isClicked(sf::RenderWindow& window) {
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+            return button.getGlobalBounds().contains(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+        }
+        return false;
+    }
+
+    void draw(sf::RenderWindow& window) {
+        window.draw(box);
+        window.draw(titleText);
+        window.draw(button);
+        window.draw(buttonText);
+    }
+};
